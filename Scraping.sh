ccb1f77e0082fd1da267e80e9627569ef22ae5dd
austinslee <alee235@ucr.edu>
2021-11-29 23:08:57 -0800

    uhhh

---
+++ Scraping.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter Stock Symbol: "
+read sym
+
+printf "$sym \n" > testinput.txt
+
+curl https://finance.yahoo.com/quote/$sym/history/ | html2text | sed -n '/Date/,$p' | sed -n '/*Close/q;p' >> testinput.txt
+
