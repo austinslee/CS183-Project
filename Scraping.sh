708ddc1b77e5fe75e69d38fe0da3c189cedb94ea
austinslee <alee235@ucr.edu>
2021-11-29 23:27:19 -0800

    small change

--- Scraping.sh
+++ Scraping.sh
@@ -3,7 +3,7 @@
 echo "Enter Stock Symbol: "
 read sym
 
-printf "$sym \n" > testinput.txt
+printf "$sym\n" > testinput.txt
 
 curl https://finance.yahoo.com/quote/$sym/history/ | html2text | sed -n '/Date/,$p' | sed -n '/*Close/q;p' >> testinput.txt
 
