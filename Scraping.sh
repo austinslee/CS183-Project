a57431c3632651c627f8d167b483eba025d15bf4
austinslee <alee235@ucr.edu>
2021-11-29 23:47:03 -0800

    added checking for invalid stocky symbol, removing dividend and - values

--- Scraping.sh
+++ Scraping.sh
@@ -5,5 +5,8 @@ read sym
 
 printf "$sym\n" > testinput.txt
 
-curl https://finance.yahoo.com/quote/$sym/history/ | html2text | sed -n '/Date/,$p' | sed -n '/*Close/q;p' >> testinput.txt
-
+if curl https://finance.yahoo.com/quote/$sym/history/ | html2text |grep 'Redirecting'; then
+	echo "Invalid Stock Symbol!"
+else
+	curl https://finance.yahoo.com/quote/$sym/history/ | html2text | sed -n '/Date/,$p' | sed -n '/*Close/q;p' | sed '/Dividend/d' | sed '/-/d' >> testinput.txt
+fi
